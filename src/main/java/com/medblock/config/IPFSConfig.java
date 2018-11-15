ca2051c2c09e402288b94e127080a0c7e7f76fd0
Rahul Soni <soni_rahul@live.com>
2018-11-15 18:54:51 +0530

    some jsch changes

---
+++ src/main/java/com/medblock/config/IPFSConfig.java
@@ -0,0 +1,14 @@
+package com.medblock.config;
+
+import io.ipfs.api.IPFS;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IPFSConfig {
+
+    @Bean
+    public IPFS ipfs() {
+        return new IPFS("/ip4/127.0.0.1/tcp/5001");
+    }
+}
